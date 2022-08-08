ffef00e58728eac5012709a60d2dd683f027aa0c
Hey You <heyyou@interactwith-us.australia-southeast1-b.c.atea-storage.internal>
2022-08-08 03:54:37 +0000

    Fixes after testing

--- GET-GOLANG.sh
+++ GET-GOLANG.sh
@@ -6,9 +6,19 @@
 if [ ! -f go$golang_package_version.linux-amd64.tar.gz ] ; then
     curl -O https://dl.google.com/go/go$golang_package_version.linux-amd64.tar.gz
 else
-    "Skipping download, as Go tarball already exists!"
+    echo "Skipping download, as Go tarball already exists!"
 fi
 
 
-tar xvzf go$golang_package_version.linux-amd64.tar.gz
+if [ ! -d go$golang_package_version.linux-amd64 ] ; then
+   
+   tar xvzf go$golang_package_version.linux-amd64.tar.gz
+
+   echo "Renaming go/ to go$golang_package_version.linux-amd64/"
+   mv go go$golang_package_version.linux-amd64
+else
+    echo "Go directory already exists: go$golang_package_version.linux-amd64"
+    echo "Skipping untar step"
+fi
+   
 
